d662c0f22cf3fdeed933aec158c8052526bbbfa2
Elias Juk <eliasp.juk@hotmail.com>
2020-04-16 16:19:13 -0300

    💫 Manipulação de imagens

--- index.py
+++ index.py
@@ -1,5 +1,12 @@
 from PIL import Image
 
-img = Image.open('img/hot-air-ballooning-small.jpg')
-img.rotate(45).show()
+image = Image.open("img/hot-air-ballooning.jpg")
+print(image.getpixel((200,200)) )
+
+image.show()
+
+#ROTACIONAR IMAGEM
+def rotate():
+    img = Image.open('img/hot-air-ballooning-small.jpg')
+    img.rotate(45).show()
 
